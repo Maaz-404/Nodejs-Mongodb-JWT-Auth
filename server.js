416d5d3c8818d9783a2f7f54fd3bccdb6e17abfe
maaz4 <maazmukhtar966@gmail.com>
2021-07-07 13:09:50 +0500

    Successful Stripe Backend Integration

--- server.js
+++ server.js
@@ -41,43 +41,131 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to Do-Nation api." });
 });
 
+app.use(express.static("."));
+app.use(express.json());
+
 const calculateOrderAmount = items => {
-/*
-  Replace this constant with a calculation of the order's amount
-
-  Calculate the order total on the server to prevent
-
-  people from directly manipulating the amount on the client*/
-
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
   return 1400;
-
 };
 
 app.post("/create-payment-intent", async (req, res) => {
-
   const { items } = req.body;
-
+  //receive username here just like you receive items object & store it in database in webhook post request
+  
   // Create a PaymentIntent with the order amount and currency
-
   const paymentIntent = await stripe.paymentIntents.create({
-
     amount: calculateOrderAmount(items),
-
     currency: "usd"
-
   });
 
   res.send({
-
     clientSecret: paymentIntent.client_secret
-
   });
+});
+
+app.post('/posting-data', (req, res) => {
+    
+    event = req.body;
+    
+    
+    console.log(event);
+    
+    switch (event.type) {
+
+    case 'payment_intent.succeeded':
+
+      const paymentIntent = event.data.object;
+
+      console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
+      
+      
+
+      // Then define and call a method to handle the successful payment intent.
+
+      // handlePaymentIntentSucceeded(paymentIntent);
+
+      break;
+
+    case 'payment_method.attached':
+
+      const paymentMethod = event.data.object;
+
+      // Then define and call a method to handle the successful attachment of a PaymentMethod.
+
+      // handlePaymentMethodAttached(paymentMethod);
+
+      break;
+
+    default:
+
+      // Unexpected event type
+
+      console.log(`Unhandled event type ${event.type}.`);
+
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+
+  res.send();
+    
+    
+    
+})
+
+app.post('/webhook', bodyParser.raw({type: 'application/json'}), (request, response) => {
+
+  const event = request.body;
+  // Handle the event
+
+  switch (event.type) {
+
+    case 'payment_intent.succeeded':
+
+      const paymentIntent = event.data.object;
+
+      console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
+      
+      
+
+      // Then define and call a method to handle the successful payment intent.
+
+      // handlePaymentIntentSucceeded(paymentIntent);
+
+      break;
+
+    case 'payment_method.attached':
+
+      const paymentMethod = event.data.object;
+
+      // Then define and call a method to handle the successful attachment of a PaymentMethod.
+
+      // handlePaymentMethodAttached(paymentMethod);
+
+      break;
+
+    default:
+
+      // Unexpected event type
+
+      console.log(`Unhandled event type ${event.type}.`);
+
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+
+  response.send();
 
 });
+
+
 
 // routes
 require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
+require("./app/routes/transaction.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
