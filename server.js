1819008ccb05d9fcabc5f4e7598d33775df28226
maaz4 <maazmukhtar966@gmail.com>
2021-07-10 21:50:44 +0500

    Remove Unused code

--- server.js
+++ server.js
@@ -118,53 +118,6 @@ app.post('/posting-data', (req, res) => {
     
 })
 
-app.post('/webhook', bodyParser.raw({type: 'application/json'}), (request, response) => {
-
-  const event = request.body;
-  // Handle the event
-
-  switch (event.type) {
-
-    case 'payment_intent.succeeded':
-
-      const paymentIntent = event.data.object;
-
-      console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
-      
-      
-
-      // Then define and call a method to handle the successful payment intent.
-
-      // handlePaymentIntentSucceeded(paymentIntent);
-
-      break;
-
-    case 'payment_method.attached':
-
-      const paymentMethod = event.data.object;
-
-      // Then define and call a method to handle the successful attachment of a PaymentMethod.
-
-      // handlePaymentMethodAttached(paymentMethod);
-
-      break;
-
-    default:
-
-      // Unexpected event type
-
-      console.log(`Unhandled event type ${event.type}.`);
-
-  }
-
-  // Return a 200 response to acknowledge receipt of the event
-
-  response.send();
-
-});
-
-
-
 // routes
 require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
